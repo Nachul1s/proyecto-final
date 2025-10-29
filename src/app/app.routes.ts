b211b8e2bad2b29abed105c8b0361a18469a7b3e
Nachul1s <nachosuazola@gmail.com>
2025-10-29 16:55:19 -0300

    ofnwaiop}fa}fn}+afp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,25 @@
 import { Routes } from '@angular/router';
-import { inicio } from '../app/componentes/inicio';
+//import { inicio } from '../app/componentes/inicionicio';
 import { Component } from '@angular/core';
+import { Inicio } from './componentes/inicio/inicio';
+import { Contacto } from './componentes/contacto/contacto';
+import { Hombres } from './componentes/hombres/hombres';
+import { Mujeres } from './componentes/mujeres/mujeres';
+import { Navbar } from './componentes/navbar/navbar';
+import { Ofertas } from './componentes/ofertas/ofertas';
+import { Ninos } from './componentes/ninos/ninos';
+import { Sombreros } from './componentes/sombreros/sombreros';
 
 export const routes: Routes = [
-    {path: '', component : inicio},
-    {path: 'Inicio', component : inicio},
     
-
+    {path: '', component : Inicio},
+    {path: 'Inicio', component : Inicio},
+    {path: 'Contacto', component: Contacto },
+    {path: 'Hombres', component : Hombres},
+    {path: 'Mujeres', component : Mujeres},
+    {path: 'Navbar', component : Navbar},
+    {path: 'Ofertas', component : Ofertas},
+    {path: 'Niños', component : Ninos},
+    {path: 'Sombreros', component : Sombreros}
+ 
 ];
