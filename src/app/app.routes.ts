57bf7a12875b6cf6d8f805328096f286db0fc1b5
Nachul1s <nachosuazola@gmail.com>
2025-10-29 15:20:44 -0300

    cmbio de lugar de los componentes  a app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { inicio } from '..';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component : inicio},
+    {path: 'Inicio', component : inicio},
+    
+
+];
